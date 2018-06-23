ae28104a40b6b48a32418ecfafd0a8a3da6dcf7a
hajime-te <arrows.v24@gmail.com>
2018-06-23 10:45:03 +0900

    separate method in fileio.py

--- fileio.py
+++ fileio.py
@@ -1,16 +1,17 @@
 import csv
 
+
 class CsvFile:
 
     def __init__(self, file_name):
         self.__file_name = file_name
 
     def read_record(self, record_container):
-
         with open(self.__file_name, newline='') as csv_file:
             reader = csv.DictReader(csv_file)
-            for row in reader:
-                record_container.append(dict(row))
+            self.append_record_to_container(reader, record_container)
 
-
-
+    @staticmethod
+    def append_record_to_container(reader, record_container):
+        for row in reader:
+            record_container.append(dict(row))
