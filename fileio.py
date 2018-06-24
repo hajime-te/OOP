368250b00ba8c95ec9f1240a8cb04644f002d1ff
hajime-te <arrows.v24@gmail.com>
2018-06-24 10:35:30 +0900

    add writer method

--- fileio.py
+++ fileio.py
@@ -11,7 +11,16 @@ class CsvFile:
             reader = csv.DictReader(csv_file)
             self.append_record_to_container(reader, record_container)
 
+    def write_record(self, record_container):
+        with open(self.__file_name, 'a', newline='') as csv_file:
+            field_name = list(record_container[0].keys())
+            writer = csv.DictWriter(csv_file, fieldnames=field_name)
+            writer.writeheader()
+            writer.writerows(record_container)
+
     @staticmethod
     def append_record_to_container(reader, record_container):
         for row in reader:
             record_container.append(dict(row))
+
+
