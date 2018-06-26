c33e6a428497a8999318d2c7395ecd1ce6fcc504
hajime-te <30080754+hajime-te@users.noreply.github.com>
2018-06-27 07:43:43 +0900

    add listen command method @Receptionist

--- receptionist.py
+++ receptionist.py
@@ -1,6 +1,16 @@
-
-
-
 class Receptionist:
-    # receive data from stdin
-    pass
+
+    def __init__(self):
+        # data processor
+        pass
+
+    def listen_command(self):
+        is_quit = False
+        while not is_quit:
+            is_quit, command = self.present_prompt()
+
+    @staticmethod
+    def present_prompt():
+        command = input('>>> ')
+        is_quit = True if command == 'q' else False
+        return is_quit, command
